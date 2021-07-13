5eb06da4ef894c07cc75f0e34d961e116eab40d8
Wilf <wilf@qq.com>
2021-07-13 19:50:23 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author wfhstart
+ * @create 2021-07-13 19:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
